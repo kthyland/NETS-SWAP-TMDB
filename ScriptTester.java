9488ad857c521ec6687785e098aa8851d17ba2cb
kthyland <49884904+kthyland@users.noreply.github.com>
2019-05-04 17:09:15 -0400

    Add files via upload

--- ScriptTester.java
+++ ScriptTester.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ScriptTester {
@@ -8,23 +9,6 @@ public class ScriptTester {
         ScriptGetter u = new ScriptGetter(
                 "https://gameofthronesfanon.fandom.com/wiki/Category:Transcripts");
         episodes = u.getEpisodes();
-        BooleanMatrix b = new BooleanMatrix(u);
-        int[][] mat = b.createBooleanMatrix();
-        
-//       testing if all the episodes happened
-        for(int i = 0; i < episodes.size(); i++) {
-            System.out.println(episodes.get(i));
-        }
-        
-       //testing out if the getCharactersInEpisode method works 
-        try {
-            Set<String> charactersEp1 = u.getCharactersInEpisode(episodes.get(0));
-            for(String s : charactersEp1) {
-                System.out.println(s);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
         
     }
 
